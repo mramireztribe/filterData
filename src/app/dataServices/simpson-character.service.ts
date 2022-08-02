b8a5f921238337d12ca61b41fdc036ce8b6bc028
Manny Ramirez <mramireztribe@gmail.com>
2022-08-02 13:34:57 -0700

    hotballz-dog

--- src/app/dataServices/simpson-character.service.ts
+++ src/app/dataServices/simpson-character.service.ts
@@ -19,44 +19,17 @@ export class SimpsonCharacterService {
   }
 
   filterCharacters(e: any, filteredCharacterName: any, myCharacters: any) {
-    console.log("myCharactersFoo", myCharacters)
     this.filteredCharacters2 = myCharacters.filter((d: any) => d != filteredCharacterName)
+    var isChecked = e.target.checked // Used on the line below: if(isChecked)
 
-    // return this.filteredCharacters2 // This RETURNS the data....and is CORRECT
-
-    var tempData: any = []
-
-    // this.filteredCharacters2 = this.characters.filter((d: any) => d != filteredCharacterName)
-
-    // return this.x
-
-    var isChecked = e.target.checked
-    console.log('isChecked', e)
     if (isChecked == true) {
-      console.log("TRUE BLUE", this.filteredCharacters2)
-      return this.filteredCharacters2
+      return this.filteredCharacters2 // return Filtered Data.....returns it to Component
     }
     else {
-      console.log("FALSE-A-ROO")
       var itemToReAdd: any;
-      console.log("asdfasdfasdfasdfsa", this.filteredCharacters2)
-      this.filteredCharacters2.push(filteredCharacterName)
-      console.log("with Kenny", this.filteredCharacters2)
-      // this.filteredCharacters2.forEach((newD: any) => {
-      //   console.log("newD = " + newD)
-      //   if (newD == filteredCharacterName) {
-      //     console.log("Time to RE....add: " + filteredCharacterName)
-      //   }
-      //   else {
-      //     console.log("You are screwed.......", filteredCharacterName)
-      //     this.filteredCharacters2.push(filteredCharacterName)
-      //     console.log("You just added: " + filteredCharacterName)
-      //   }
-      // });
-      return this.filteredCharacters2;
+      this.filteredCharacters2.push(filteredCharacterName) // Add to the Filtered Data
+      return this.filteredCharacters2; // Sends back to Component. 
     }
-    // return this.filteredCharacters
-
   }
 
 }
