f3d4356832c3ecfa854ca2261f7fce0d87a06954
Manny Ramirez <mramireztribe@gmail.com>
2022-08-02 06:50:00 -0700

    Code clean-up

--- src/app/components/filter-data/filter-data.component.ts
+++ src/app/components/filter-data/filter-data.component.ts
@@ -11,15 +11,21 @@ export class FilterDataComponent implements OnInit {
   constructor(private _characterService: SimpsonCharacterService) { }
 
   myCharacters: any = []
+  topMenuCharacters: any = []
 
   ngOnInit(): void {
+
+    //SET COMPONENT VARIABLES = DataServiceCalls()
+    this.topMenuCharacters = this._characterService.getAllCharacters();
     this.myCharacters = this._characterService.getAllCharacters()
   }
 
+  /* The filterCharacter() function Below gets called from the HTML page on the (change) event....it filters the data
+     based off which "checkbox" the user clicked
+  */
   filterCharacters(e: any) {
     var nameToFilter = e.target.defaultValue
     this.myCharacters = this._characterService.filterCharacters(e, nameToFilter)
-    console.log("returned data", this.myCharacters)
   }
 
 }
