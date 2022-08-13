d9a16cb2a0a2fbb5ecbcbcdbdb38fdadb55a34e0
Manny Ramirez <mramireztribe@gmail.com>
2022-08-13 05:21:12 -0700

    added 'accordion-item' Component

--- src/app/components/lodash-customized-filters/components/accordion-items/accordion-items.component.ts
+++ src/app/components/lodash-customized-filters/components/accordion-items/accordion-items.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accordion-items',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accordion-items.component.scss']
 })
 export class AccordionItemsComponent implements OnInit {
+
+  @Input() filter: any;
+  @Input() header: any;
 
   constructor() { }
 
