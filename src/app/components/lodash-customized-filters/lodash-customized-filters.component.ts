9d28beb83099844ff04feb5bb34fcb82ee1fd097
Manny Ramirez <mramireztribe@gmail.com>
2022-08-13 11:41:16 -0700

    test

--- src/app/components/lodash-customized-filters/lodash-customized-filters.component.ts
+++ src/app/components/lodash-customized-filters/lodash-customized-filters.component.ts
@@ -24,19 +24,19 @@ export class LodashCustomizedFiltersComponent implements OnInit {
   constructor(private _characterDataservice: SimpsonCharacterService) { }
 
   ngOnInit(): void {
-    var data = this._characterDataservice.getAllCharacters();
+    var simpsonFamilyCharacters = this._characterDataservice.getAllCharacters();
     var twoFamiliesData = this._characterDataservice.getTwoFamilies(); console.log("two families", twoFamiliesData);
 
-    this.defaultFilter = _.defaults(data); console.log("defaultFilter", this.defaultFilter);
+    this.defaultFilter = _.defaults(simpsonFamilyCharacters); console.log("defaultFilter", this.defaultFilter);
     // FIRST line below this comment (_.chunk) WORKS!!!! just cant loop through it the same way......because it chunks out multiple objects, still in order
-    this.chunkFilter = _.chunk(data, 1); console.log("chunkFilter", this.chunkFilter);
-    this.compactFilter = _.compact(data); console.log("compactFilter", this.compactFilter);
-    this.concatFilter = _.concat(data, { name: "Sideshow Bob", orderId: 555 }); console.log("concatFilter", this.concatFilter);
+    this.chunkFilter = _.chunk(simpsonFamilyCharacters, 1); console.log("chunkFilter", this.chunkFilter);
+    this.compactFilter = _.compact(simpsonFamilyCharacters); console.log("compactFilter", this.compactFilter);
+    this.concatFilter = _.concat(simpsonFamilyCharacters, { name: "Sideshow Bob", orderId: 555 }); console.log("concatFilter", this.concatFilter);
     this.differenceFilter = _.difference(twoFamiliesData.families, twoFamiliesData[0], twoFamiliesData[1]); console.log("differenceFilter", this.differenceFilter);
-    this.dropFilter = _.drop(data, 3); console.log("dropFilter", this.dropFilter);
-    this.dropRightFilter = _.dropRight(data, 2); console.log("dropRightFilter", this.dropRightFilter)
-    this.dropWhileFilter = _.dropWhile(data, ['gender', 'Male']); console.log("dropWhileFilter", this.dropWhileFilter);
-    this.fillFilter = _.fill(data, { orderId: 0, name: "Maggie", gender: "Female" }); console.log("fill Filter", this.fillFilter);
-    this.findIndexFilter = _.findIndex(data, { orderId: 3, name: "Bart", gender: "Male" }); console.log("this.findIndexFilter", this.findIndexFilter);
+    this.dropFilter = _.drop(simpsonFamilyCharacters, 3); console.log("dropFilter", this.dropFilter);
+    this.dropRightFilter = _.dropRight(simpsonFamilyCharacters, 2); console.log("dropRightFilter", this.dropRightFilter)
+    this.dropWhileFilter = _.dropWhile(simpsonFamilyCharacters, ['gender', 'Male']); console.log("dropWhileFilter", this.dropWhileFilter);
+    this.fillFilter = _.fill(simpsonFamilyCharacters, { orderId: 0, name: "Maggie", gender: "Female" }); console.log("fill Filter", this.fillFilter);
+    this.findIndexFilter = _.findIndex(simpsonFamilyCharacters, { orderId: 3, name: "Bart", gender: "Male" }); console.log("this.findIndexFilter", this.findIndexFilter);
   }
 }
